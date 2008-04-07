15f45deb2f923095a47742f11dd8546ce48c2333
Adam Dickmeiss <adam@indexdata.dk>
2008-04-07 21:18:35 +0200

    Removed bundled yaz-source. build.sh uses yaz-config..

--- libyaz4j/build.sh
+++ libyaz4j/build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 JNI_DIR=${JAVA_HOME}/include
+set -x
+YAZ_CONFIG=yaz-config
+YAZ_PREFIX=`${YAZ_CONFIG} --prefix`
+YAZ_CFLAGS=`${YAZ_CONFIG} --cflags`
+YAZ_lIBS=`${YAZ_CONFIG} --libs`
 
-swig -I"include" -I"../dependencies/yaz-2.1.28/include" -outdir ../src/org/yaz4j/jni -package org.yaz4j.jni -o src/libyaz4j.cpp -c++ -java libyaz4j.i
+swig -I"include" -I"${YAZ_PREFIX}/include" -outdir ../src/org/yaz4j/jni -package org.yaz4j.jni -o src/libyaz4j.cpp -c++ -java libyaz4j.i
 
-g++ -I"include" -I"$JNI_DIR" -I"$JNI_DIR/linux" -I"../dependencies/yaz-2.1.28/include" -c -Wall -o obj/libyaz4j.o src/libyaz4j.cpp
+g++ -I"include" -I"$JNI_DIR" -I"$JNI_DIR/linux" ${YAZ_CFLAGS} -c -Wall -o obj/libyaz4j.o src/libyaz4j.cpp
 
-g++ -I"include" -I"$JNI_DIR" -I"$JNI_DIR/linux" -I"../dependencies/yaz-2.1.28/include" -c -Wall -o obj/zoom-extra.o src/zoom-extra.cpp
+g++ -I"include" -I"$JNI_DIR" -I"$JNI_DIR/linux" ${YAZ_CFLAGS} -c -Wall -o obj/zoom-extra.o src/zoom-extra.cpp
 
-g++ -L"../dependencies/yaz-2.1.28/lib" -shared obj/libyaz4j.o obj/zoom-extra.o -lyaz -o lib/libyaz4j.so
+g++ -shared obj/libyaz4j.o obj/zoom-extra.o  -o lib/libyaz4j.so ${YAZ_LIBS}
 
 
