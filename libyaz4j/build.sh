428d895d6dc31b784e953ea935d662cfc4a4bd43
Adam Dickmeiss <adam@indexdata.dk>
2009-09-21 15:31:54 +0200

    Use Maven for building yaz4j.

--- libyaz4j/build.sh
+++ libyaz4j/build.sh
@@ -6,7 +6,7 @@ YAZ_PREFIX=`${YAZ_CONFIG} --prefix`
 YAZ_CFLAGS=`${YAZ_CONFIG} --cflags`
 YAZ_LIBS=`${YAZ_CONFIG} --libs`
 
-swig -I"include" -I"${YAZ_PREFIX}/include" -outdir ../src/org/yaz4j/jni -package org.yaz4j.jni -o src/libyaz4j.cpp -c++ -java libyaz4j.i
+swig -I"include" -I"${YAZ_PREFIX}/include" -outdir ../src/main/java/org/yaz4j/jni -package org.yaz4j.jni -o src/libyaz4j.cpp -c++ -java libyaz4j.i
 
 g++ -fPIC -I"include" -I"$JNI_DIR" -I"$JNI_DIR/linux" ${YAZ_CFLAGS} -c -Wall -o obj/libyaz4j.o src/libyaz4j.cpp
 
