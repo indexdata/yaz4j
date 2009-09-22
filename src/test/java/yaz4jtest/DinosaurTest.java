12f33b352c7edaab68bb1a5fc0197efa911265d7
Adam Dickmeiss <adam@indexdata.dk>
2009-09-22 15:21:40 +0200

    Add other test case (uses render).

---
+++ src/test/java/yaz4jtest/DinosaurTest.java
@@ -0,0 +1,23 @@
+package yaz4jtest;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.yaz4j.*;
+
+/**
+ * @author adam
+ */
+public class DinosaurTest {
+    @Test
+    public void test() {
+        Connection con = new Connection("z3950.loc.gov:7090/voyager", 0);
+        assertNotNull(con);
+        con.setSyntax("usmarc");
+        PrefixQuery pqf = new PrefixQuery("@attr 1=7 0253333490");
+        assertNotNull(pqf);
+        ResultSet set = con.Search(pqf);
+        assertNotNull(set);
+        Record rec = set.getRecord(0);
+	assertNotNull(rec);
+	// System.out.println(rec.render());
+    }
+}
